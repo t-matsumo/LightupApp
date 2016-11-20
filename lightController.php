c5b21512355776cbd107911f822bc53d9fcc5cdd
Tatsuki_Matsumoto <tatsukimatsumo@gmail.com>
2016-11-20 14:02:13 +0900

    deploy

--- lightController.php
+++ lightController.php
@@ -2,13 +2,12 @@
   $minute = $_POST["minute"];
   $hour = $_POST["hour"];
 
-  $fp = fopen("/etc/cron.d/lightup", "w");
-  fwrite($fp, "#!/bin/sh\n");
-  fwrite($fp, "SHELL=/bin/bash\n");
-  fwrite($fp, "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games\n");
-  fwrite($fp, "HOME=/\n");
-  fwrite($fp, "${minute} ${hour} * * * tatsuki sh /home/tatsuki/on_light.sh\n");
-  fclose($fp);
+  $outputFile = "/etc/cron.d/lightup";
+  exec('sudo sh -c "echo \'#!/bin/sh\n\' >> ' .$outputFile .'"');
+  exec('sudo sh -c "echo \'SHELL=/bin/bash\n\' >> ' .$outputFile .'"');
+  exec('sudo sh -c "echo \'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games\n\' >> ' .$outputFile .'"');
+  exec('sudo sh -c "echo \'HOME=/\n\' >> ' .$outputFile .'"');
+  exec('sudo sh -c "echo \'' .${minute} .' ' .${hour} .' * * * tatsuki sh /home/tatsuki/on_light.sh\n\' >> ' .$outputFile .'"');
 //  0  0    *   *   *   sh /home/tatsuki/off_light.sh
 
   echo("${minute}  ${hour}    *   *   *   tatsuki sh /home/tatsuki/on_light.sh");
