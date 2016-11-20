7b33daab6f0efccef9fe3d72a02770721b1a76e4
Tatsuki_Matsumoto <tatsukimatsumo@gmail.com>
2016-11-20 14:05:46 +0900

    deploy

--- lightController.php
+++ lightController.php
@@ -2,7 +2,8 @@
   $minute = $_POST["minute"];
   $hour = $_POST["hour"];
 
-  $outputFile = "/etc/cron.d/lightup";
+  $outputFile = /var/www/html/aaa
+  //$outputFile = "/etc/cron.d/lightup";
   exec('sudo sh -c "echo \'#!/bin/sh\n\' >> ' .$outputFile .'"');
   exec('sudo sh -c "echo \'SHELL=/bin/bash\n\' >> ' .$outputFile .'"');
   exec('sudo sh -c "echo \'PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games\n\' >> ' .$outputFile .'"');
