c59387e316fffd320b78d34393cc2d3e03c4e004
Tatsuki_Matsumoto <tatsukimatsumo@gmail.com>
2016-11-20 14:35:10 +0900

    deploy

--- lightController.php
+++ lightController.php
@@ -6,10 +6,10 @@
   fwrite($fp, "#!/bin/sh\n");
   fwrite($fp, "SHELL=/bin/bash\n");
   fwrite($fp, "PATH=/sbin:/bin:/usr/sbin:/usr/bin\n");
-  fwrite($fp, "${minute} ${hour} * * * tatsuki sh /home/tatsuki/on_light.sh\n");
+  fwrite($fp, "${minute} ${hour} * * * sh /home/tatsuki/on_light.sh\n");
   fclose($fp);
   exec("crontab cron");
 
 //  0  0    *   *   *   sh /home/tatsuki/off_light.sh
 
-  echo("${minute}  ${hour}    *   *   *   tatsuki sh /home/tatsuki/on_light.sh");
+  echo("${minute}  ${hour}    *   *   *   sh /home/tatsuki/on_light.sh");
