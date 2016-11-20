303b0a8f52b7c90f5d9d5d4f59dc8d50502c3f0f
Tatsuki_Matsumoto <tatsukimatsumo@gmail.com>
2016-11-20 14:27:16 +0900

    deploy

--- lightController.php
+++ lightController.php
@@ -2,14 +2,13 @@
   $minute = $_POST["minute"];
   $hour = $_POST["hour"];
 
-  chmod("/etc/cron.d/lightup", 0666);
-  $fp = fopen("/etc/cron.d/lightup", "w");
+  $fp = fopen("/var/www/html/lightupapp/cron", "w");
   fwrite($fp, "#!/bin/sh\n");
   fwrite($fp, "SHELL=/bin/bash\n");
   fwrite($fp, "PATH=/sbin:/bin:/usr/sbin:/usr/bin\n");
   fwrite($fp, "${minute} ${hour} * * * tatsuki sh /home/tatsuki/on_light.sh\n");
   fclose($fp);
-  chmod("/etc/cron.d/lightup", 0644);
+  exec("crontab cron");
 
 //  0  0    *   *   *   sh /home/tatsuki/off_light.sh
 
