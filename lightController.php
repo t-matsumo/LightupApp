799355d4e6b970ae3fe9a8d717bcea792ab56957
Tatsuki_Matsumoto <tatsukimatsumo@gmail.com>
2016-11-20 12:20:20 +0900

    aaa

--- lightController.php
+++ lightController.php
@@ -3,7 +3,10 @@
   $hour = $_POST["hour"];
 
   $fp = fopen("/etc/cron.d/lightup", "w");
-  fwrite($fp, "${minute} ${hour} * * * tatsuki sh /home/tatsuki/on_light.sh");
+  fwrite($fp, "#!/bin/sh\n");
+  fwrite($fp, "SHELL=/bin/bash\n");
+  fwrite($fp, "PATH=/sbin:/bin:/usr/sbin:/usr/bin\n");
+  fwrite($fp, "${minute} ${hour} * * * tatsuki sh /home/tatsuki/on_light.sh\n");
   fclose($fp);
 //  0  0    *   *   *   sh /home/tatsuki/off_light.sh
 
