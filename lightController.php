0c4a8fa9e6c84a945e1eef7aa546d27d073650b0
Tatsuki_Matsumoto <tatsukimatsumo@gmail.com>
2016-11-20 13:15:17 +0900

    deploy

--- lightController.php
+++ lightController.php
@@ -5,7 +5,8 @@
   $fp = fopen("/etc/cron.d/lightup", "w");
   fwrite($fp, "#!/bin/sh\n");
   fwrite($fp, "SHELL=/bin/bash\n");
-  fwrite($fp, "PATH=/sbin:/bin:/usr/sbin:/usr/bin\n");
+  fwrite($fp, "PATH=/usr/local/sbin:/usr/local/bin:/usr/sbin:/usr/bin:/sbin:/bin:/usr/local/games:/usr/games\n");
+  fwrite($fp, "HOME=/\n");
   fwrite($fp, "${minute} ${hour} * * * tatsuki sh /home/tatsuki/on_light.sh\n");
   fclose($fp);
 //  0  0    *   *   *   sh /home/tatsuki/off_light.sh
